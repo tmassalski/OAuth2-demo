2bc2a59057f35c156dcd88174723fce13a838f13
Tomasz Massalski <tomasz.massalski@gmail.com>
2020-08-26 14:37:08 +0200

    Initial commit

---
+++ src/test/java/pl/tmassalski/OAuth2/demo/OAuth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tmassalski.OAuth2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
